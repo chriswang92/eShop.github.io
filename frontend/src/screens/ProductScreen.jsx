bcf7d4a8ec642a74367c2b8591d9598e73cd40d5
chris wang <wcg19920705@gmail.com>
2021-05-12 10:47:02 -0400

    Fetching Products From React (useEffect)

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = ({ match }) => {
-  console.log(match);
-  const product = products.find(p => p._id === match.params.id);
-  console.log(product);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  });
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
@@ -54,7 +61,13 @@ const ProductScreen = ({ match }) => {
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                  <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add To Cart</Button>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
