1089d34937c8043b4935e9f5d55446187551c139
chris wang <wcg19920705@gmail.com>
2021-06-09 15:12:06 -0400

    MongoDB set up

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,36 @@
 import express from 'express'; // 'require' is common js which is traditionally what nodejs is use, on frontend we use 'import' which is es modules
 import dotenv from 'dotenv';
+import connectedDB from './config/db.js'; // on backend nodejs file, when import another js file, the '.js' needs to be added 
 import products from './data/products.js';
 
-dotenv.config()
+dotenv.config();
+
+connectedDB();
 
 const app = express();
 
 app.get('/', (req, res) => {
-  // send string response to client
-  res.send('API is running23...');
+    // send string response to client
+    res.send('API is running23...');
 });
 
 app.get('/api/products', (req, res) => {
-  // send JSON response to client
-  // products here is a js array with objects
-  // res.json or res.send will convert the object to JSON content-type
-  res.json(products);
+    // send JSON response to client
+    // products here is a js array with objects
+    // res.json or res.send will convert the object to JSON content-type
+    res.json(products);
 });
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+    PORT,
+    console.log(
+        `server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
